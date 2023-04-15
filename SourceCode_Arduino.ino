3ab72abf8c86704e1f6c580060b9a2b8b28df854
Christopher Gyubum Kim <kgb2872@gmail.com>
2023-04-14 23:07:29 -0400

    Add files via upload
    Code for ESP8266 to interact with Google Firebase

---
+++ SourceCode_Arduino.ino
@@ -0,0 +1,147 @@
+/*
+File: Source Code of Arduino
+Course: TPJ655 NCD
+Name: Sunghyun Park, Gyubum Kim
+Student #: 109226209, 101492171
+Date: 2023 - 04 - 07
+*/
+
+
+#include <Servo.h>
+#include <Arduino.h>
+#if defined(ESP32)
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#elif defined(PICO_RP2040)
+#include <WiFi.h>
+#include <FirebaseESP8266.h>
+#include <Firebase_ESP_Client.h>
+#endif
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "BELL879"
+#define WIFI_PASSWORD "AFD4519DCFFA"
+
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyA4w_wr6fsM14Rcc6rwDL4hRTxn_R1NOzc"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "nodemcu8266-9edee-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "kgb2872@gmail.com"
+#define USER_PASSWORD "Rla1234@"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+unsigned long count = 0;
+Servo s1;  
+byte servoPin = 4;
+
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+WiFiMulti multi;
+#endif
+
+void setup()
+{
+  Serial.begin(115200);
+
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+  multi.addAP(WIFI_SSID, WIFI_PASSWORD);
+  multi.run();
+#else
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+#endif
+
+  Serial.print("Connecting to Wi-Fi");
+  unsigned long ms = millis();
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    if (millis() - ms > 10000)
+      break;
+#endif
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  // The WiFi credentials are required for Pico W
+  // due to it does not have reconnect feature.
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+  config.wifi.clearAP();
+  config.wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
+#endif
+
+  Firebase.begin(&config, &auth);
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library
+  Firebase.reconnectWiFi(true);
+
+  Firebase.setDoubleDigits(5);
+
+  pinMode(4,OUTPUT);
+  s1.attach(servoPin);
+}
+
+void loop()
+{
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0))
+  {
+    sendDataPrevMillis = millis();
+
+    if (Firebase.getBool(fbdo, "/test/FEED_STATUS"))
+    {
+      bool ledStatus = fbdo.boolData();
+
+      if (ledStatus)
+      {
+        Serial.println("Servo Motor ON");
+        s1.write(0);
+        // Change the value of "/test/FEED_STATUS" to false
+        Firebase.setBool(fbdo, "/test/FEED_STATUS", false);
+      }
+      else
+      {
+        Serial.println("Servo Motor OFF");
+        s1.write(90);
+      }
+    }
+    FirebaseJson json;
+    Serial.println();
+    count++;
+  }
+}
